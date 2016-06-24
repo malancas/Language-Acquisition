df050b6e32b0a65250fbf997a9decfc8630f9c5b
malancas <malancaster0@gmail.com>
2016-06-24 14:02:30 +0000

    Removed unused functions. Updated function name and variable names to better reflect their purposes

--- src/research_clojure/core.clj
+++ src/research_clojure/core.clj
@@ -1,20 +1,27 @@
 (ns research-clojure.core)
 (require ['clojure.string :as 'str])
+(require '[clojure.data.csv :as csv])
 
 ;Used to read in the sentence file
-;Each line is an element
-(defn read-file [file]
-  (str/split-lines (slurp file)))
+;Creates a vector of vectors
+(defn readFile
+  [file]
+   (with-open [rdr (clojure.java.io/reader file)] (vec (csv/read-csv rdr :separator \tab))))
 
+(defn chooseSentence
+  [x gID]
+  (if (= gID (get x 0))
+    ))
+
+(defn chooseSelectSentences
+  [file gID]
+  (map (chooseSentence gID) file))
 
 (defn -main
   "I don't do a whole lot."
   []
-  (println "Hello, World!")
-  ;(def x (read-file "tests.txt"))
-  (def x (str/split "543  dfsfsdfds" #" "))
-  (println x)
-  (println (get x 1))
-  (println (get x 0))
-  (println (get x 2)))
+  (println "Load file from memory")
+  (def allSentences (readFile "tests.txt"))
+  (def selectedSentences (chooseSelectSentences allSentences "611")))
+
 
