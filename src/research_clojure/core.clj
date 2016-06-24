a407281cb48e2ecc097ad4a414370a9bffc95b6c
malancas <malancaster0@gmail.com>
2016-06-24 15:08:22 +0000

    Added function to choose specific functions based on grammar id. Updated function names

--- src/research_clojure/core.clj
+++ src/research_clojure/core.clj
@@ -8,20 +8,33 @@
   [file]
    (with-open [rdr (clojure.java.io/reader file)] (vec (csv/read-csv rdr :separator \tab))))
 
-(defn chooseSentence
+(defn isSentenceNeeded
   [x gID]
-  (if (= gID (get x 0))
-    ))
+  (println (get x 0))
+  (= gID (get x 0)))
 
-(defn chooseSelectSentences
+(defn chooseSentences
   [file gID]
-  (map (chooseSentence gID) file))
+  (loop [remainingSentences file chosenSentences []]
+    (if (empty? remainingSentences)
+      chosenSentences
+      (let [[sentence & remaining] remainingSentences]
+        (recur remaining
+               (into chosenSentences
+                     (if (isSentenceNeeded sentence gID)
+                       (set [sentence]))))))))
+
+
 
 (defn -main
   "I don't do a whole lot."
   []
   (println "Load file from memory")
   (def allSentences (readFile "tests.txt"))
-  (def selectedSentences (chooseSelectSentences allSentences "611")))
+  ;(println (get (get allSentences 0) 0))
+  (def selectedSentences (chooseSentences allSentences "611"))
+  (println (count selectedSentences))
+  (println (get selectedSentences 0))
+  )
 
 
