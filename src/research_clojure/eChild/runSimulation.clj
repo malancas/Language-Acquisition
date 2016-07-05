16fcdb1ab3954bd2971e7e62c1b3ab3ab794aff3
malancas <malancaster0@gmail.com>
2016-07-05 13:06:24 +0000

    Renamed processingSentences to runSimulation. Moved eChild related functions from runSimulation.clj to Child.clj

---
+++ src/research_clojure/eChild/runSimulation.clj
@@ -0,0 +1,35 @@
+(ns research-clojure.eChild.processingSentences)
+(require 'research-clojure.eChild.Child)
+(refer 'research-clojure.eChild.Child)
+(require ['clojure.string :as 'str])
+
+
+(defn writeResults [lines]
+ ;need to be rewritten
+)
+
+
+(defn doesChildLearnGrammar?
+  "Runs sentence consuming functions until the 
+  correct grammar is learned or the maximum number
+  of sentences are processed. Needs infoList, grammar, expected grammar"
+  [sentences max_num]
+  (def grammarLearned (atom false))
+  (def sentenceCount (atom 0))
+
+  (while (and (not @grammarLearned) (< @sentenceCount max_num))
+    (def infoListAndState (consumeSentence (rand-nth sentences)))
+    (def eChildData (conj eChildData [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1]))
+    (def currGrammarAndState (setParameters (get eChildData 0) (get eChildData 2)))
+    (swap! grammarLearned (isGrammarLearned? (get currGrammarAndState 1)))
+    (swap! sentenceCount inc)) 
+  (writeResults currGrammarAndState))
+
+
+(defn runSimulation
+  [sentences max_eChildren max_sentences]
+  (def count (atom 0))
+  (while (< @count max_eChildren)
+    (doesChildLearnGrammar? sentences max_sentences)
+    (swap! count inc)))
+
